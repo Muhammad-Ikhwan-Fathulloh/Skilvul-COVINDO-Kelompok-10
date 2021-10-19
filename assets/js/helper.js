c2f339ce5d07a8730008f2a0002cceb146a1f2b3
Muhammad-Ikhwan-Fathulloh <muhammadikhwanfathulloh17@gmail.com>
2021-10-19 21:35:02 +0700

    Menambah helper

---
+++ assets/js/helper.js
@@ -0,0 +1,11 @@
+export const getNews = async() => {
+    // EDIT HERE
+};
+
+export const getHospital = async() => {
+    // EDIT HERE
+};
+
+export const getMaps = async() => {
+    // EDIT HERE
+};
